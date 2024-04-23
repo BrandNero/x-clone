0f98deeece2128c7deea814657fbaf7f1d0fdc27
Sergio rodriguez <rodriguezruizsergio@gmail.com>
2024-04-23 11:30:35 -0400

    added character count for the tweet area

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,23 @@
+$(document).ready(() => {
+  const maxChar = 140;
+
+  $("#tweet-text").on("input", function() {
+    // Number of characters
+    const charsLength = this.value.length;
+
+    // gets current characters in text
+    const characters = maxChar - charsLength;
+
+    // Find the counter element in html side
+    const counterRef = $(this).parent("form").find(".counter");
+
+    counterRef.text(characters);
+
+    //turn red when going over 140 charachters
+    if (characters < 0) {
+      counterRef.addClass('overCount');
+    } else {
+      counterRef.removeClass('overCount');
+    }
+  });
+});
