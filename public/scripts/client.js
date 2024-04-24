d6fd64c96aff489f18e4e0ed049f22aca64125d7
Sergio rodriguez <rodriguezruizsergio@gmail.com>
2024-04-24 11:47:09 -0400

    made the create tweet and render tweet functions

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,56 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const createTweetElement = function(tweetData) {
+  const $tweet = $(`<article class="tweet"></article>`);
+  return $tweet;
+};
+const tweetData =  {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1713735675804
+};
+
+const $tweet = createTweetElement(tweetData);
+console.log($tweet);
+$('#tweets-container').append($tweet);
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+const renderTweets = function(data) {
+  let tweets;
+  for (const users of data) {
+    tweets = createTweetElement(users);
+  }
+  return tweets;
+};
+
+renderTweets(data);
