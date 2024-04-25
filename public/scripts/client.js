ae2e055bba4678bdecf175ef1527f3884acb186c
Sergio rodriguez <rodriguezruizsergio@gmail.com>
2024-04-25 11:40:10 -0400

    made it cross-site scripting safe

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,21 +5,36 @@
  */
 
 $(() => {
+  const escapeHTML = function(str) {
+    // Uses the replace method on the string to search for special HTML characters
+    // The /[&<>"']/g is a regular expression that matches characters that need escaping in HTML
+    return str.replace(/[&<>"']/g, function(match) {
+
+      switch (match) { // will replace character with it's HTML equivalent
+      case '&': return '&amp;';
+      case '<': return '&lt;';
+      case '>': return '&gt;';
+      case '"': return '&quot;';
+      case "'": return '&#39;';
+      default: return match;
+      }
+    });
+  };
   // create the tweets box
   const createTweetElement = function(data) {
     // Article creation
     const timeAgo = timeago.format(data.created_at);
-    const $tweet = `
+    const $tweet = $(`
     <article class="tweet">
       <div class="profile">
       <div class="name-handles">
-      <img src="${data.user.avatars}">
-          <span class="tweet-user-name">${data.user.name}</span>           
+      <img src="${escapeHTML(data.user.avatars)}">
+          <span class="tweet-user-name">${escapeHTML(data.user.name)}</span>           
           </div>
-        <span>${data.user.handle}</span>
+        <span>${escapeHTML(data.user.handle)}</span>
       </div>
         <div class="tweet-content">
-        ${data.content.text}
+        ${escapeHTML(data.content.text)}
         </div>
         <footer>
           <span class="tweet-symbols">
@@ -30,9 +45,11 @@ $(() => {
         <span class="timestamp">${timeAgo}</span>
         </footer>
         </article>
-        `;
+        `);
     return $tweet;
   };
+  const $tweetJQueryObject = $($tweet);
+  $tweetJQueryObject.find('.tweet-handle').text(data.user.handle);
   const renderTweets = (tweets) => {
     for (const tweet of tweets) {
       const $tweetEntry = createTweetElement(tweet);
@@ -72,6 +89,7 @@ $(() => {
   });
   
   const loadTweets = function() {
+    $('.tweets-box').empty();
     $.ajax({
       method: "get",
       url: "/tweets",
